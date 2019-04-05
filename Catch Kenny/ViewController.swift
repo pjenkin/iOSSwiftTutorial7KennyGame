ac66e68be947e75b9b44f9ec21b5664cbae13b08
Peter Jenkin <peternjenkin@gmail.com>
2019-04-05 15:23:31 +0100

    7-55 For each image, added gesture, recognizer & enabled interaction, and function to increase score

---
+++ Catch Kenny/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Catch Kenny
+//
+//  Created by Peter Jenkin on 05/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScores))
+        
+        kenny1.addGestureRecognizer(recognizer1)
+        kenny2.addGestureRecognizer(recognizer2)
+        kenny3.addGestureRecognizer(recognizer3)
+        kenny4.addGestureRecognizer(recognizer4)
+        kenny5.addGestureRecognizer(recognizer5)
+        kenny6.addGestureRecognizer(recognizer6)
+        kenny7.addGestureRecognizer(recognizer7)
+        kenny8.addGestureRecognizer(recognizer8)
+        kenny9.addGestureRecognizer(recognizer9)
+
+        kenny1.isUserInteractionEnabled = true
+        kenny2.isUserInteractionEnabled = true
+        kenny3.isUserInteractionEnabled = true
+        kenny4.isUserInteractionEnabled = true
+        kenny5.isUserInteractionEnabled = true
+        kenny6.isUserInteractionEnabled = true
+        kenny7.isUserInteractionEnabled = true
+        kenny8.isUserInteractionEnabled = true
+        kenny9.isUserInteractionEnabled = true
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func increaseScores ()
+    {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
+    }
+
+
+}
+
